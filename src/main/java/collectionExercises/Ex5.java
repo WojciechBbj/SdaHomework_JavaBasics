d8a3d2388309a1de8ae2097c5dac49b3a0a92454
uzytkownik1 <uzytkownik@wp.pl>
2020-06-22 17:29:12 +0200

    Ukonczyłem całe zadanie 5

--- src/main/java/collectionExercises/Ex5.java
+++ src/main/java/collectionExercises/Ex5.java
@@ -8,11 +8,25 @@ public class Ex5 {
     public static List<String> listOfNames = new ArrayList<String>();
 
     public static void addNames(int howMuch) {
-        System.out.println("5a, adding 5 names");
+        System.out.println("5a, adding " + howMuch + " names");
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < listOfNames.size(); i++) {
+        for (int i = 0; i < howMuch; i++) {
             System.out.print("Podaj imie: ");
             listOfNames.add(scanner.next());
         }
+    }
+
+    public static void showList() {
+        int i = 0;
+        System.out.println("5a, List od names:");
+        while (i < listOfNames.size()) {
+            System.out.println(listOfNames.get(i));
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        addNames(5);
+        showList();
     }
 }
