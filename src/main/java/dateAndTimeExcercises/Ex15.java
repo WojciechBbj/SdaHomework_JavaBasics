285b00b5a265bcfb09b1a692c536cc0072378b2f
uzytkownik1 <uzytkownik@wp.pl>
2020-06-23 21:27:50 +0200

    Rozwiązano zadanie 15

---
+++ src/main/java/dateAndTimeExcercises/Ex15.java
@@ -0,0 +1,32 @@
+package dateAndTimeExcercises;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Ex15 {
+    ZoneId zone1 = ZoneId.of("Europe/Warsaw");
+    LocalDate localDate = LocalDate.now(zone1);
+    LocalTime localTime = LocalTime.now(zone1);
+    LocalDateTime localDateTime = LocalDateTime.now(zone1);
+
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyy ' roku, ' EEEE hh:mm:ss");
+
+    public Ex15() {
+        shoDateTime();
+    }
+
+    public void shoDateTime() {
+        System.out.println("Czas przed ustawieniem formatu czasu: " + localDateTime);
+        System.out.println("Czas w Bydgoszczy: " + localDateTime.format(dateTimeFormatter));
+
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Ex15 currentTIme = new Ex15();
+    }
+}
