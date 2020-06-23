664b744ddeecef037ebf4922b1fc99f2312a029f
uzytkownik1 <uzytkownik@wp.pl>
2020-06-23 23:48:43 +0200

    Dodano klasę Address, konstrukory klasy Person, gettery i settery

---
+++ src/main/java/OOP_Excercises/Address.java
@@ -0,0 +1,17 @@
+package OOP_Excercises;
+
+public class Address {
+    public String street;
+    public String city;
+    public String country;
+    public String flatNo;
+    public String homeNo;
+
+    public Address (String street, String city, String country, String flatNo, String homeNo) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.flatNo = flatNo;
+        this.homeNo = homeNo;
+    }
+}
--- src/main/java/OOP_Excercises/Person.java
+++ src/main/java/OOP_Excercises/Person.java
@@ -5,6 +5,7 @@ public class Person {
     private String name;
     private String surname;
     private int age;
+    Address address;
 
     public Person() {
 
@@ -14,6 +15,45 @@ public class Person {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    public Person(String name, String surname, int age, Address addAddress) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = addAddress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 
     public void introduce() {
