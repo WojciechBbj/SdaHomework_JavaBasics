7e152f8454922946cd5f85ebb094984738aff51e
uzytkownik1 <uzytkownik@wp.pl>
2020-06-23 18:57:48 +0200

    Rozwiązano zadanie 11

--- src/main/java/dateAndTimeExcercises/Ex10.java
+++ src/main/java/dateAndTimeExcercises/Ex10.java
@@ -1,8 +1,20 @@
 package dateAndTimeExcercises;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.ZoneId;
 
 public class Ex10 {
-    public LocalDate localDate 
+
+    ZoneId zone1 = ZoneId.of("Europe/Berlin");
+    public static LocalDate localDate = LocalDate.now();
+
+    public static void showLocalDate() {
+        System.out.println("Actual date: " + localDate);
+    }
+
+    public  static void main(String[] args) {
+
+        showLocalDate();
+    }
+
 }
