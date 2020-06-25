85283ada7a174935468714315aadaac63e8d618b
uzytkownik1 <uzytkownik@wp.pl>
2020-06-25 22:10:00 +0200

    Rozwiązanie zadania 6

---
+++ src/main/java/java_exercises_3_sda/Car.java
@@ -0,0 +1,9 @@
+package java_exercises_3_sda;
+
+public class Car implements Movable {
+
+    @Override
+    public String move() {
+        return "idę";
+    }
+}
---
+++ src/main/java/java_exercises_3_sda/Movable.java
@@ -0,0 +1,5 @@
+package java_exercises_3_sda;
+
+public interface Movable {
+    public String move();
+}
