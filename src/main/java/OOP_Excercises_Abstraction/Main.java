da163675d6e2f75c77c04b31e7325b18959a49bb
uzytkownik1 <uzytkownik@wp.pl>
2020-06-25 17:05:48 +0200

    Poprawiono błąd w pisowni

--- src/main/java/OOP_Excercises_Abstraction/Main.java
+++ src/main/java/OOP_Excercises_Abstraction/Main.java
@@ -11,8 +11,8 @@ public class Main {
         int random1;
         int random2;
         for (int i = 0; i < howManyRectangular; i++) {
-            random1 = (int)(Math.random()*10);
-            random2 = (int)(Math.random()*10);
+            random1 = (int) (Math.random() * 10);
+            random2 = (int) (Math.random() * 10);
             Rectangle rectangle = new Rectangle(random1, random2);
             figures.add(rectangle);
         }
@@ -21,9 +21,18 @@ public class Main {
     public static void addSquareToList(int howManySquare) {
         int random;
         for (int i = 0; i < howManySquare; i++) {
-            random = (int)(Math.random()*10);
+            random = (int) (Math.random() * 10);
             Square square = new Square(random);
             figures.add(square);
+        }
+    }
+
+    public static void addCircleToList(int howManyCircle) {
+        int random;
+        for (int i = 0; i < howManyCircle; i++) {
+            random = (int) (Math.random() * 10);
+            Circle circle = new Circle(random);
+            figures.add(circle);
         }
     }
 
@@ -40,7 +49,7 @@ public class Main {
         System.out.print("Podaj liczbę prostokątów do wygenerowania: ");
         int numberRectangular = scanner.nextInt();
 
-        System.out.print("Podaj liczbę kwadrató do wygenerowania: ");
+        System.out.print("Podaj liczbę kwadratów do wygenerowania: ");
         int numberSquare = scanner.nextInt();
 
         System.out.print("Podaj liczbę prostokątó do wygenerowania: ");
@@ -48,6 +57,7 @@ public class Main {
 
         addRectangularToList(numberRectangular);
         addSquareToList(numberSquare);
+        addCircleToList(numberCircle);
         showList();
     }
 }
