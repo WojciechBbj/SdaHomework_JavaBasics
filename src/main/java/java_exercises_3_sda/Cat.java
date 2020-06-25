c74c60e74bf4352fc83063d109d5c63899871fb2
Wojtek_linus <wojtekbabij@wp.pl>
2020-06-25 19:49:31 +0200

    Rozwiazano zadania 1-4

--- src/main/java/java_exercises_3_sda/Cat.java
+++ src/main/java/java_exercises_3_sda/Cat.java
@@ -1,15 +1,26 @@
 package java_exercises_3_sda;
 
+import java.util.Scanner;
+
 public class Cat {
 
-    String catName;
+    private String catName;
 
     public Cat(String catName) {
         this.catName = catName;
     }
 
     public void makeSound() {
-        System.out.println(catName + "makes miau miau !!!");
+        System.out.println(catName + " makes miau miau !!!");
+    }
+
+    public void eatMouse() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ile myszy zjadł " + catName + " ?");
+        int howManyMouse = scanner.nextInt();
+
+        System.out.println(catName + ": Zjadłem " + howManyMouse + " myszy\n");
     }
     
 }
--- src/main/java/java_exercises_3_sda/Main.java
+++ src/main/java/java_exercises_3_sda/Main.java
@@ -1,4 +1,21 @@
 package java_exercises_3_sda;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Mruczek");
+        Cat cat2 = new Cat("Filemon");
+        Cat cat3 = new Cat("Bonifacy");
+
+        Cat[] catTable = {cat1, cat2, cat3};
+
+        for (int i = 0; i < catTable.length; i++) {
+            catTable[i].makeSound();
+        }
+
+        cat1.eatMouse();
+        cat2.eatMouse();
+        cat3.eatMouse();
+    }
 }
